505403e55d75d8e42e16b8d40a663c4c1adf8a89
rchen53 <rchen53@binghamton.edu>
2020-03-16 22:36:07 -0400

    pseudocodes only

---
+++ script.js
@@ -0,0 +1,8 @@
+//a search button that listens to a botton submit and also a working eventlistener for the search button
+//create an array of items for cities
+//create variables for weather api to get results
+//create a function that handles returning api information of temperture, humidity, windspeed, uv index
+//create a function that handles going through the array of cities to return information
+//append it to body
+//local storage to save result of searches
+
