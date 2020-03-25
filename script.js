167ab15ae5cd60f293a8e9e2472c5731fa6459a6
rchen53 <rchen53@binghamton.edu>
2020-03-25 17:04:12 -0400

    apikey, couple variables, still need to work on function, still need to work on giving ids to index page

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+var today = todayDate();
+var presentDate = today.getDate() + "." + (today.getMonth() + 1) + today.getFullYear();
+var cities = [JSON.parse(localStorage.getItem("cities"))];
+var APIkey = "b9c9094662e2b96ef8e56de156eb952e";
+var city = $("#search").val();
+
+$("button").on("click", function(e){
+    e.preventDefault();
+    generateForcast();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //a search button that listens to a botton submit and also a working eventlistener for the search button
 //create an array of items for cities
 //create variables for weather api to get results
